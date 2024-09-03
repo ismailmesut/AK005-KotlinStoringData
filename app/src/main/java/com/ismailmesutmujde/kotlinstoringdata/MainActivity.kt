bfd99908e1b9bfa404a98af4714d8a600512d2ff
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 14:51:36 +0300

    updated: reading data

--- app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ismailmesutmujde.kotlinstoringdata
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,7 @@ import com.ismailmesutmujde.kotlinstoringdata.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingMainActivity : ActivityMainBinding
+    lateinit var sharedPref : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +19,14 @@ class MainActivity : AppCompatActivity() {
         val view = bindingMainActivity.root
         setContentView(view)
 
+        // Shared Preferences - XML : Key-Value
+        sharedPref = this.getSharedPreferences("com.ismailmesutmujde.kotlinstoringdata", Context.MODE_PRIVATE)
+
     }
 
     fun save (view: View) {
-
+        val myAge = bindingMainActivity.editText.text.toString()
+        //bindingMainActivity.textView.text = "Your age : " + myAge
     }
 
     fun delete(view: View) {
