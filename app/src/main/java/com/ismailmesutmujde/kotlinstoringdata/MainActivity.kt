87b83cf6d3de2ca32dac267a90050cdd8d64d5cd
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 14:41:41 +0300

    updated: storing data

--- app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.ismailmesutmujde.kotlinstoringdata
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinstoringdata.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMainActivity : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
+
+    }
+
+    fun save (view: View) {
+
+    }
+
+    fun delete(view: View) {
 
     }
 }
