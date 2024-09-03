1e84bb73b5e993c408a38992ed2967ad39b25876
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 15:02:57 +0300

    updated: delete data

--- app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoringdata/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingMainActivity : ActivityMainBinding
     lateinit var sharedPref : SharedPreferences
+    var ageFromPref : Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +23,31 @@ class MainActivity : AppCompatActivity() {
         // Shared Preferences - XML : Key-Value
         sharedPref = this.getSharedPreferences("com.ismailmesutmujde.kotlinstoringdata", Context.MODE_PRIVATE)
 
+        ageFromPref = sharedPref.getInt("age", -1)
+
+        if (ageFromPref == -1) {
+            bindingMainActivity.textView.text = "Your age : "
+        } else {
+            bindingMainActivity.textView.text = "Your age : ${ageFromPref}"
+        }
+
+
     }
 
     fun save (view: View) {
-        val myAge = bindingMainActivity.editText.text.toString()
-        //bindingMainActivity.textView.text = "Your age : " + myAge
+        val myAge = bindingMainActivity.editText.text.toString().toIntOrNull()
+        if (myAge != null) {
+            bindingMainActivity.textView.text = "Your age : ${myAge}"
+            sharedPref.edit().putInt("age", myAge).apply()
+        }
+
     }
 
     fun delete(view: View) {
-
+        ageFromPref = sharedPref.getInt("age", -1)
+        if (ageFromPref != -1) {
+            sharedPref.edit().remove("age").apply()
+            bindingMainActivity.textView.text = "Your age : "
+        }
     }
 }
